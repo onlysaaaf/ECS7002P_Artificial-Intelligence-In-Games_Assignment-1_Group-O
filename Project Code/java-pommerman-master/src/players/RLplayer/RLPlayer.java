b6f359480cf410669fa94c98ec648a37d9c5eaec
bk305 <b.khaleque@se16.qmul.ac.uk>
2019-10-15 16:59:01 +0100

    Added actions array and arraylist to keep a track of current actions available from current state and updated skeleton methods

--- Project Code/java-pommerman-master/src/players/RLplayer/RLPlayer.java
+++ Project Code/java-pommerman-master/src/players/RLplayer/RLPlayer.java
@@ -5,12 +5,16 @@ import players.Player;
 import players.heuristics.AdvancedHeuristic;
 import players.heuristics.CustomHeuristic;
 import utils.Types;
+
+import javax.swing.*;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class RLPlayer extends Player {
 
     private Random random;
     private GameState currentState;
+    private Types.ACTIONS[] actions;
     /**
      * Default constructor, to be called in subclasses (initializes player ID and random seed for this agent.
      *     * @param seed - random seed for this player.
@@ -19,6 +23,8 @@ public class RLPlayer extends Player {
     protected RLPlayer(long seed, int pId) {
         super(seed, pId);
         random = new Random();
+        ArrayList<Types.ACTIONS> actionsList = Types.ACTIONS.all();
+        actions = new Types.ACTIONS[actionsList.size()];
     }
 
     @Override
@@ -38,9 +44,11 @@ public class RLPlayer extends Player {
 
     private int calculateReward(){
         int newQ = random.nextInt();
+        int selectedAction = random.nextInt(actions.length);   //Get current state, execute action randomly or with neural net
+        int reward = 0;
         //TODO
 
-        //Get current state, execute action randomly or with neural net
+
         //Get reward, get new state prime, update Q value with bellman equation
         //Keep original state, action, state reached after action, reward and if game ended or not in storage
         return newQ;
@@ -48,8 +56,12 @@ public class RLPlayer extends Player {
 
     }
 
-    void setCurrentGameState(GameState gs)
+    private void setCurrentGameState(GameState gs)
     {
-        //TODO
+        currentState = gs;
+    }
+
+    private GameState getCurrentState(){
+        return  currentState;
     }
 }
