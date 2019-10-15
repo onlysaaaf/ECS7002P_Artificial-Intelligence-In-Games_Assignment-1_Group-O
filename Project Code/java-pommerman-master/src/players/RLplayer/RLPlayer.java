626712e375453bcdf48fa529b91897b53ee058d7
bk305 <b.khaleque@se16.qmul.ac.uk>
2019-10-15 17:06:51 +0100

    Added skeleton while loop to calculateReward method in RL

--- Project Code/java-pommerman-master/src/players/RLplayer/RLPlayer.java
+++ Project Code/java-pommerman-master/src/players/RLplayer/RLPlayer.java
@@ -4,6 +4,8 @@ import core.GameState;
 import players.Player;
 import players.heuristics.AdvancedHeuristic;
 import players.heuristics.CustomHeuristic;
+import players.mcts.SingleTreeNode;
+import utils.ElapsedCpuTimer;
 import utils.Types;
 
 import javax.swing.*;
@@ -46,11 +48,21 @@ public class RLPlayer extends Player {
         int newQ = random.nextInt();
         int selectedAction = random.nextInt(actions.length);   //Get current state, execute action randomly or with neural net
         int reward = 0;
-        //TODO
+
+        boolean stop = false;
+
+        while(!stop){
+
+            GameState state = currentState.copy();
+            ElapsedCpuTimer elapsedTimerIteration = new ElapsedCpuTimer();
+            stop = true;
+            //Get reward, get new state prime, update Q value with bellman equation
+            //Keep original state, action, state reached after action, reward and if game ended or not in storage
+            //stop after X amount of time
+            //TODO
+        }
 
 
-        //Get reward, get new state prime, update Q value with bellman equation
-        //Keep original state, action, state reached after action, reward and if game ended or not in storage
         return newQ;
 
 
