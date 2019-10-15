0f5a89d2eab7e6769e68c6e5b320727ff0eab2cd
bk305 <b.khaleque@se16.qmul.ac.uk>
2019-10-15 16:19:02 +0100

    Added default constructor and skeleton methods for RLPlayer

--- Project Code/java-pommerman-master/src/players/RLplayer/RLPlayer.java
+++ Project Code/java-pommerman-master/src/players/RLplayer/RLPlayer.java
@@ -1,4 +1,33 @@
 package players.RLplayer;
 
-public class RLPlayer {
+import core.GameState;
+import players.Player;
+import utils.Types;
+
+public class RLPlayer extends Player {
+
+    /**
+     * Default constructor, to be called in subclasses (initializes player ID and random seed for this agent.
+     *
+     * @param seed - random seed for this player.
+     * @param pId  - this player's ID.
+     */
+    protected RLPlayer(long seed, int pId) {
+        super(seed, pId);
+    }
+
+    @Override
+    public Types.ACTIONS act(GameState gs) {
+        return null;
+    }
+
+    @Override
+    public int[] getMessage() {
+        return new int[0];
+    }
+
+    @Override
+    public Player copy() {
+        return null;
+    }
 }
