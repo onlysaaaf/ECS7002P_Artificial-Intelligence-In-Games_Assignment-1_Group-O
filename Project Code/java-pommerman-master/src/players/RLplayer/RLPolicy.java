30df5391a361ba2cd114998ee122cab82c61f555
bk305 <b.khaleque@se16.qmul.ac.uk>
2019-10-17 17:45:53 +0100

    Added skeleton methods for RLPolicy

--- Project Code/java-pommerman-master/src/players/RLplayer/RLPolicy.java
+++ Project Code/java-pommerman-master/src/players/RLplayer/RLPolicy.java
@@ -3,10 +3,59 @@ package players.RLplayer;
 import core.GameState;
 import utils.Types;
 
+import java.lang.reflect.Array;
+
 public class RLPolicy {
     int[] dimSize;
     double[] qValues;
     private Object qValuesTable;
     GameState[] states;
     Types.ACTIONS[] actions;
+
+    Types.ACTIONS getBestAction(){
+        return null;
+    }
+
+    GameState getNextState(){
+        return null;
+    }
+
+    private double[] myQValues( int[] state ) {
+
+
+        return null;
+    }
+
+    public double[] getQValuesAt( int[] state ) {
+
+        int i;
+        Object curTable = qValuesTable;
+        double[] returnValues;
+
+
+        return null;
+    }
+
+
+    public void setQValue( int[] state, int action, double newQValue ) {
+
+        qValues = myQValues( state );
+        Array.setDouble( qValues, action, newQValue );
+    }
+
+    public double getMaxQValue( int[] state ) {
+
+        double maxQ = -Double.MAX_VALUE;
+
+        return maxQ;
+    }
+
+    public double getQValue( int[] state, int action ) {
+
+        double qValue = 0;
+
+
+        return qValue;
+    }
+
 }
