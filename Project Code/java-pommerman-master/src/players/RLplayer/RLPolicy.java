0b02bcaeef8129b8764c9de3c28a787e6df92b90
bk305 <b.khaleque@se16.qmul.ac.uk>
2019-10-21 16:05:53 +0100

    Added roll method to RLPolicy to simulate future states

--- Project Code/java-pommerman-master/src/players/RLplayer/RLPolicy.java
+++ Project Code/java-pommerman-master/src/players/RLplayer/RLPolicy.java
@@ -5,6 +5,7 @@ import utils.Types;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class RLPolicy {
     int[] dimSize;
@@ -12,6 +13,7 @@ public class RLPolicy {
     private Object qValuesTable;
     GameState[] states;
     Types.ACTIONS[] actions;
+    Random r = new Random();
 
     private double alpha = 0.1; //Learning rate
     private double gamma = 0.3; //Eagerness
@@ -22,13 +24,49 @@ public class RLPolicy {
         ArrayList<Types.ACTIONS> actionsList = Types.ACTIONS.all();
         actions = new Types.ACTIONS[actionsList.size()];
         double maxVal = Double.MAX_VALUE;
+        double eval = 0;
 
         GameState copy = g.copy();
+
+        for(int i =0; i<actions.length; ++i){
+            GameState next = (roll(copy,actions[i]));
+            evaluate(next);
+
+        }
+
 
         //pick action to move to state that has maximum Q value
         //TODO
 
         return maxVal;
+    }
+
+    private GameState roll(GameState gs, Types.ACTIONS act)
+    {
+        //Simple, all random first, then my position.
+        int nPlayers = 4;
+        Types.ACTIONS[] actionsAll = new Types.ACTIONS[4];
+        int playerId = gs.getPlayerId() - Types.TILETYPE.AGENT0.getKey();
+
+        for(int i = 0; i < nPlayers; ++i)
+        {
+            if(playerId == i)
+            {
+                actionsAll[i] = act;
+            }else {
+                int actionIdx = r.nextInt(gs.nActions());
+                actionsAll[i] = Types.ACTIONS.all().get(actionIdx);
+            }
+        }
+
+        gs.next(actionsAll);
+
+        return gs;
+
+    }
+
+    private Double evaluate(GameState gs){
+        return null;
     }
 
 
