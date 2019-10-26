3e2197550261d74863c67511547d0950fb2756ac
BKhaleque <b.khaleque@se16.qmul.ac.uk>
2019-10-26 17:50:56 +0100

    Added some shit to RL Learner

--- Project Code/java-pommerman-master/src/players/RLplayer/RLLearner.java
+++ Project Code/java-pommerman-master/src/players/RLplayer/RLLearner.java
@@ -15,7 +15,9 @@ public class RLLearner {
     private GameState currentState;
     private Random random;
     private Types.ACTIONS[] actions;
-
+    private RLParams params;
+    private ArrayList<Double> qValues;
+    private ArrayList<GameState> states;
 
 
     private RLPolicy policy;
@@ -26,32 +28,61 @@ public class RLLearner {
         ArrayList<Types.ACTIONS> actionsList = Types.ACTIONS.all();
         actions = new Types.ACTIONS[actionsList.size()];
         policy = new RLPolicy();
+         params = new RLParams();
+         qValues = new ArrayList<>();
+         states = new ArrayList<>();
+
     }
 
     public int learn(){
-        //apply qlearning algorithm here
         //todo
         return 0;
     }
 
     //calculate reward for state here
-    private double calculateQ(){
-        int reward = 0;
-
+    private double calculateQ() {
+        int index = 0;
+        int numIters = 0;
         boolean stop = false;
-
+        int acumTimeTaken = 0;
+        int avgTimeTaken = 0;
+        long remaining = 0;
+        int remainingLimit = 5;
+        int fmCallsCount = 0;
+        double newQ = Double.MIN_VALUE;
         GameState state = currentState.copy();
-        ElapsedCpuTimer elapsedTimerIteration = new ElapsedCpuTimer();
-        int numOfActions = actions.length;
-        int index = random.nextInt(numOfActions);
-        double newQ =  policy.getPolicyFromState(currentState);
-            //stop = true;
-            //Get reward, get new state prime, update Q value with bellman equation
-            //Keep original state, action, state reached after action, reward and if game ended or not in storage
-            //stop after X amount of time
-            //TODO
-        return newQ;
 
+
+        ElapsedCpuTimer ect = new ElapsedCpuTimer();
+        ect.setMaxTimeMillis(40);
+
+
+        while (!stop) {
+
+            int numOfActions = actions.length;
+            newQ = policy.getPolicyFromState(currentState);
+            ElapsedCpuTimer elapsedTimerIteration = new ElapsedCpuTimer();
+            //TODO
+
+            if (params.stop_type == params.STOP_TIME) {
+                numIters++;
+                acumTimeTaken += (elapsedTimerIteration.elapsedMillis());
+                avgTimeTaken = acumTimeTaken / numIters;
+                remaining = ect.remainingTimeMillis();
+                stop = remaining <= 2 * avgTimeTaken || remaining <= remainingLimit;
+            } else if (params.stop_type == params.STOP_ITERATIONS) {
+                numIters++;
+                stop = numIters >= params.num_iterations;
+            } else if (params.stop_type == params.STOP_FMCALLS) {
+                fmCallsCount += params.rollout_depth;
+                stop = (fmCallsCount + params.rollout_depth) > params.num_fmcalls;
+            }
+
+
+
+
+        }
+        return newQ;
 
     }
 }
